2a446d3104c87c45ba77cee34e36fc278f024614
lincoln <sleeved.quills.0u@icloud.com>
2026-08-22 08:54:58 +0200

    style(ui): format label primitive

--- packages/ui/src/components/label.tsx
+++ packages/ui/src/components/label.tsx
@@ -3,12 +3,18 @@ import { cn } from '../utils'
 
 export type LabelProps = LabelHTMLAttributes<HTMLLabelElement>
 
-export const Label = forwardRef<HTMLLabelElement, LabelProps>(({ className, ...props }, ref) => (
-  <label
-    className={cn('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70', className)}
-    ref={ref}
-    {...props}
-  />
-))
+export const Label = forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <label
+      className={cn(
+        'text-sm font-medium leading-none peer-disabled:cursor-not-allowed',
+        'peer-disabled:opacity-70',
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  ),
+)
 
 Label.displayName = 'Label'
